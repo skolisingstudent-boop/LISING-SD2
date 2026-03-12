00dece413fe9841934eb3a1c50efdda09e80eba4
skolisingstudent-boop <skolising.student@ua.edu.ph>
2026-03-12 11:57:58 +0800

    Add files via upload

---
+++ session14/index.js
@@ -0,0 +1,85 @@
+// this is how  we print an output using js
+// console.log (stringcontent variable);
+
+
+console.log("Hello World!");
+
+// to use comment we need to use -> comment
+// this is a single line comment
+
+/* this is a multi line comment
+we can write multiple lines of comment here
+and it will be ignored by the js engine
+*/
+
+// [section] variables
+// acts as a storage for data which can be modified afterwards after excepts constant variables
+
+//[ssection] data types
+
+//numeric data type
+
+
+
+let age;
+let height = 160;
+
+console.log(height);
+
+//js is losw-type pl
+console.log(height);
+
+console.    log (  height  )   ;
+
+console.   
+ log (
+    height  )   ;
+
+      // synchronous pl -> js
+
+    let gwa = 99.75;
+    console.log(gwa);
+
+    // charcter data types
+
+    let firstName = "John";
+
+    console.log(firstName);
+
+    //type of to check the data type of a variable  
+
+    console.log(typeof firstName);
+    console.log(typeof gwa);
+    console.log(typeof height);
+
+    // text casing in js
+    // camelCase -> this is camelcasing
+    // snake_case -> this-is-snake-casing
+    // pascalCase -> This-Is-Pascal-Casing
+    // kebab-case -> this-is-kebab-casing
+    // pscal kebab case -> this -is-pascal-kebab-case
+
+
+    //boolean data type
+
+    let isRaining = true;
+    let isSunny = false;
+    let isCloudy = true;
+
+    console.log(typeof isRaining);
+    console.log(typeof isSunny);
+
+    console.log(isRaining);
+
+    //conatination of string
+
+    let province = "pampanga";
+
+    console.log("i live in " + province);
+
+//type-coecion
+//concatenation of string and number or data type conversion to string
+
+
+    console.log("my overall grade is " + gwa);
+
